48795cd12e3a4a3fb5197209e3fac236ac875294
LucasTroaca <troacalucasgabriel@gmail.com>
2026-03-16 01:43:59 -0300

    first commit

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,5 +1,7 @@
 from pages.checkout_page import CheckoutPage
 
 def test_finish_purchase(checkout_page):
-    checkout_page.checkout_one_to_two()
     checkout_page.expect_checkout_one()
+    checkout_page.checkout_one_to_two()
+    checkout_page.expect_checkout_two()
+
